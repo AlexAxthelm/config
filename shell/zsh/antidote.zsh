34ba109add2ba435878a2106644c36a6c7335285
Alex Axthelm <aaxthelm@RMI.org>
2023-10-21 16:04:54 +0200

    Add powerline10k prompt

--- shell/zsh/antidote.zsh
+++ shell/zsh/antidote.zsh
@@ -7,8 +7,6 @@ export ANTIDOTE_HOME="$XDG_CONFIG_HOME/shell/zsh/plugins"
 if [ ! -d "$ZSH_ANTIDOTE_DIR" ]; then
   echo "$ZSH_ANTIDOTE_DIR does not exist. Cloning form GitHub."
   git clone --depth=1 https://github.com/mattmc3/antidote.git "$ZSH_ANTIDOTE_DIR"
-else
-  echo "$ZSH_ANTIDOTE_DIR does exist."
 fi
 
 source $ZSH_ANTIDOTE_DIR/antidote.zsh
