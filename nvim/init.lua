e86f361f014695d3cf56ffcb1d895767085fe3ae
Alex Axthelm <aaxthelm@RMI.org>
2025-11-19 21:01:15 +0100

    overhaul nvim config (vimscript to lua) and start fresh

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+-- ~/.config/nvim/init.lua
+-- Minimal, blank-slate Neovim configuration
+
+-- Leader key (optional, but useful for custom maps)
+vim.g.mapleader = " "
+
+-- Basic settings for a sane baseline
+vim.opt.number = true       -- show line numbers
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true -- enable true color support
